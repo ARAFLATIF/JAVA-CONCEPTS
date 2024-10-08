0553123ceba177db829ce92350b68415970ef58c
Araf Ibne Latif <124219901+ARAFLATIF@users.noreply.github.com>
2024-10-07 23:01:56 -0500

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+class HelloWorld {
+    static int age = 17;
+	public static void main(String[] arguments) {
+	    System.out.println("I am " + age + " years old!!" + " and I am very happy about it");
+	}
+}
+//the classic hello world but I have implemented the public statuc and used that in my code. Simple and yet a bit different. 
