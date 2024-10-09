4e656572875eee10e114ff4ffa907fb04cc6e1e9
Araf Ibne Latif <124219901+ARAFLATIF@users.noreply.github.com>
2024-10-08 19:07:39 -0500

    Create using_cotains_methods_in_strings.java

---
+++ Strings/using_cotains_methods_in_strings.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Declare and initialize a String variable
+        String string = "The sky is pink.";
+        
+        // Use the contains() method to check if the string contains "pink"
+        // The contains() method returns a boolean value (true or false)
+        // Print the result of this check to the console
+        System.out.println(string.contains("pink"));
+    }
+}
