7739c0304f53aacdb4048f54e0f2908d2e0a37b5
Araf Ibne Latif <124219901+ARAFLATIF@users.noreply.github.com>
2024-10-16 01:02:02 -0500

    Update AudioBook.java

--- OOP/AudioBook.java
+++ OOP/AudioBook.java
@@ -1,1 +1,20 @@
+public class AudioBook extends Book {
+    private int duration;
 
+    public AudioBook(String title, String author, double price, int duration) {
+        super(title, author, price);
+        this.duration = duration;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    @Override
+    public void displayDetails() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: $" + price);
+        System.out.println("Duration: " + duration + " minutes");
+    }
+}
